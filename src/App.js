b0b3c8426bb74b3b7c51e5cccf3355b6c6e7c7ac
Henrique S. Hinke <hhinke@gmail.com>
2021-08-20 18:29:05 -0300

    Splitting components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+import Post from './Post';
+import Header from './Header';
+
 function App() {
   return (
-    <div>
-      <h1>Component App</h1>
-      <h2>Subtitulo</h2>
-    </div>
+    <>
+      <Header title="JStack's Blog">
+        <h2>Posts da Semana</h2>  
+      </Header>
+
+      <hr />
+
+      <Post title="Título da Notícia 01" 
+            subtitle="Subtítulo da Notícia 01"
+      />
+      <Post title="Título da Notícia 02" 
+            subtitle="Subtítulo da Notícia 02"
+      />
+      <Post title="Título da Notícia 03" 
+            subtitle="Subtítulo da Notícia 03"
+      />
+
+    </>
   )
 }
 
