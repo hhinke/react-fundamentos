b1d9b622100a5420fb585942cdc6aa5167a35bd1
Henrique S. Hinke <hhinke@gmail.com>
2021-08-25 17:39:15 -0300

    Added styled-components and reorganized folders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import App from './App';
-
-import './index.scss';
+import App from './components/App';
 
 ReactDom.render(
   <App />,
